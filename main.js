2e04f3eab0690c46f020d46281c344ea6a8fe873
Jonathan20102 <110703440+Jonathan20102@users.noreply.github.com>
2023-07-07 21:03:19 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var first_name="Ranbir";
+console.log(first_name);
+var Last_name="Kanadoo"
+console.log(Last_name);
+console.log(first_name.concat(Last_name));
+var a=10;
+var b=5;
+var sum=a+b;
+console.log(sum);
+var difference=a-b;
+console.log(difference);
+var product=a*b;
+console.log(product);
+var divide=a/b;
+console.log(divide);
